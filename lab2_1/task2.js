54ccf2592b1631e07be248db8349531e89f88469
MaxSharshon <sharshonmax@gmail.com>
2025-05-20 22:11:04 +0300

    Add lab2_1

---
+++ lab2_1/task2.js
@@ -0,0 +1,13 @@
+function printEvenNumbers() {
+  let number = 0;
+
+  do {
+    if (number % 2 === 0) {
+      console.log(number);
+    }
+    number++;
+  } while (number <= 100);
+}
+
+// Виклик функції
+printEvenNumbers();
